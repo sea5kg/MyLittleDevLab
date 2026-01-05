d36960fb1247d777adef4d6fa750e84b22676547
Evgenii Sopov <mrseakg@gmail.com>
2026-01-05 12:24:56 +0700

    fix jquery ajax request application/json

--- data/html/js/index.js
+++ data/html/js/index.js
@@ -4,10 +4,11 @@ function test_api() {
         url: "/api", // Server endpoint URL
         method: "POST",            // HTTP method (GET, POST, PUT, DELETE, etc.)
         dataType: "json",          // Expected data type in the response (xml, json, script, html)
-        data: {                    // Data to be sent to the server
+        contentType: "application/json",
+        data: JSON.stringify({
             method: "John",
             location: "Boston"
-        },
+        }),
         success: function(data) {  // Function to run if the request succeeds
             console.log("Data received:", data);
             // Process the data here (e.g., update UI)
